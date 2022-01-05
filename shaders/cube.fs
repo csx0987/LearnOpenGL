5082d4a6e8b76cc0c4ac8b6be3da2a4f7572fa2c
chenshengxin <chenshengxin@bytedance.com>
2022-01-05 09:57:41 +0800

    feat: 增加高光

--- shaders/cube.fs
+++ shaders/cube.fs
@@ -7,6 +7,7 @@ in vec3 worldPos;
 uniform vec3 lightPos; 
 uniform vec3 lightColor;
 uniform vec3 objectColor;
+uniform vec3 viewPos;
 
 void main()
 {
@@ -19,7 +20,15 @@ void main()
     vec3 lightDir = normalize(lightPos - worldPos);
     float diff = max(dot(norm, lightDir), 0.0);
     vec3 diffuse = diff * lightColor;
+
+    // specular
+    float specularStrength = 0.5;
+    vec3 viewDir = normalize(viewPos - worldPos);
+    vec3 reflectDir = reflect(-lightDir, norm);
+
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
+    vec3 specular = specularStrength * spec * lightColor;
             
-    vec3 result = (ambient + diffuse) * objectColor;
+    vec3 result = (ambient + diffuse + specular) * objectColor;
     FragColor = vec4(result, 1.0);
 }
